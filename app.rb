494887149a272247a355dbe179a7b083aef7185b
sebas_z <zanabria.s@pucp.pe>
2020-05-08 20:09:53 -0500

    Reading list complete

--- app.rb
+++ app.rb
@@ -1,43 +1,46 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "json"
-require_relative "models/Book"
-require_relative "modules/HTTPManagement"
-require_relative "constants/constants"
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
+require_relative 'models/Book'
+require_relative 'modules/HTTPManagement'
+require_relative 'constants/constants'
 
 helpers HTTPManagement
 
-get "/" do
-  erb :index, :layout => false
+get '/' do
+  erb :index, layout: false
 end
 
-get "/search" do
+get '/search' do
   @books = []
 
   unless params.empty?
-    data = get_api("volumes?q=#{params["query"].gsub(" ", "+")}&maxResults=8")
-    @books = data["items"]
+    data = get_api("volumes?q=#{params['query'].gsub(' ', '+')}&maxResults=8")
+    @books = data['items']
   end
-  
+
   erb :search
 end
 
-get "/books" do
-
+get '/books' do
+  erb :reading_list
 end
 
-post "/books" do
-  data = JSON.parse(params["book-data"])
-  book = Book.new(data["id"], data["cover"], data["title"], data["author"], data["status"], Time.now)
+post '/books' do
+  data = JSON.parse(params['book-data'])
+  book = Book.new(data['id'], data['cover'], data['title'], data['author'], data['status'], Time.now)
   book.save
-  redirect url("/books")
+  redirect url('/books')
 end
 
-get "/books/:id" do
-  book = get_api("volumes/#{params["id"]}")
-  erb :book, locals:{book: book}
+get '/books/:id' do
+  book = get_api("volumes/#{params['id']}")
+  erb :book, locals: { book: book }
 end
 
+<<<<<<< HEAD
 post "/books/:id/edit" do
   book = Book.find(params["id"])
   p params
